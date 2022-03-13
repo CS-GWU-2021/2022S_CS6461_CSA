a44c2175ccf398fc3bb49632a366a2b7f7a5a436
Tenphun0503 <tenzinphuntsok0503@gmail.com>
2022-03-12 23:27:29 -0500

    input file can be modified in main py file

--- Homework/Project2/CISC6461/CISC6461.py
+++ Homework/Project2/CISC6461/CISC6461.py
@@ -3,8 +3,18 @@ from GUI import *
 from system import System
 
 if __name__ == '__main__':
+
+    file_dir = './program.txt'
+    pc_default = 6
+
+    #file_dir = './program1.txt'
+    #pc_default = int('100',16)
+
+    #file_dir = './ipl.txt'
+    #pc_default = int('1010',2)
+
     # initialize the system
-    sys = System()
+    sys = System(file_dir, pc_default)
 
     # initialize a tkinter window
     window = Tk()
